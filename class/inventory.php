aadecc1c8e99ff353e41306c1df2730393f0f201
Yohei Kokubo <yheihei0126@gmail.com>
2018-05-24 01:14:41 +0000

    リクエストXMLをクラスオブジェクトをぶち込むと自動で生成する処理を追加

--- class/inventory.php
+++ class/inventory.php
@@ -1,12 +1,11 @@
 <?php
-
-class Point
+class Inventory
 {
   // xmlで自動生成するときに順番通りに要素が並ぶ
   // RMS APIは順番を組み替えると400が返ってくるので注意すること
-  public $pointRate;
-  public $pointRateStart;
-  public $pointRateEnd;
+  public $inventoryCount; //在庫数
+  public $normalDeliveryDateId; // 在庫あり時納期管理番号
+  public $backorderDeliveryDateId; // 在庫切れ時納期管理番号
   
   function __construct() {
       
--- class/itemInventory.php
+++ class/itemInventory.php
@@ -1,12 +1,11 @@
 <?php
-
-class Point
+class ItemInventory
 {
   // xmlで自動生成するときに順番通りに要素が並ぶ
   // RMS APIは順番を組み替えると400が返ってくるので注意すること
-  public $pointRate;
-  public $pointRateStart;
-  public $pointRateEnd;
+  public $inventoryType; // 在庫タイプ
+  public $inventories; // Inventoryクラスのオブジェクトをarrayで追加していく
+  public $inventoryQuantityFlag;
   
   function __construct() {
       
