355255509bd32bff39e40b016b5a59a995b28066
Yohei Kokubo <yheihei0126@gmail.com>
2018-05-24 04:59:24 +0000

    バリエーション商品の新規登録サンプル(在庫タイプ2：項目選択肢別在庫設定)

--- class/inventory.php
+++ class/inventory.php
@@ -4,8 +4,14 @@ class Inventory
   // xmlで自動生成するときに順番通りに要素が並ぶ
   // RMS APIは順番を組み替えると400が返ってくるので注意すること
   public $inventoryCount; //在庫数
+  public $childNoVertical; // 項目選択肢別在庫設定 時のバリエーションの縦軸
+  public $childNoHorizontal; // 項目選択肢別在庫設定 時のバリエーションの横軸
+  public $optionNameVertical;
+  public $optionNameHorizontal;
   public $normalDeliveryDateId; // 在庫あり時納期管理番号
   public $backorderDeliveryDateId; // 在庫切れ時納期管理番号
+  public $isRestoreInventoryFlag; // 在庫戻し設定 キャンセル時に在庫をもどすか
+  public $images; // SKU画像情報リスト InventoryImageクラスのオブジェクトをarrayで追加していく
   
   function __construct() {
       
--- class/itemInventory.php
+++ class/itemInventory.php
@@ -5,8 +5,10 @@ class ItemInventory
   // RMS APIは順番を組み替えると400が返ってくるので注意すること
   public $inventoryType; // 在庫タイプ
   public $inventories; // Inventoryクラスのオブジェクトをarrayで追加していく
-  public $inventoryQuantityFlag;
-  
+  public $verticalName; // 項目選択肢別在庫用縦軸選択肢項目名
+  public $horizontalName; // 項目選択肢別在庫用横軸選択肢項目名
+  public $inventoryQuantityFlag; // 在庫数表示 在庫タイプが「1：通常在庫設定」の時のみ設定できる
+  public $inventoryDisplayFlag; // 項目選択肢別在庫用残り表示閾値 在庫タイプが「2：項目選択肢別在庫設定 」の時のみ設定できる -1で表示  0で表示しない 1以上で△表示
   function __construct() {
       
   }
