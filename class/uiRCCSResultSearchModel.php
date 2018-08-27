18770ac1336fb21a9d1417ba949e98f66d938ab3
Yohei Kokubo <yheihei0126@gmail.com>
2018-08-27 05:07:02 +0000

    一括決済結果取得機能（getRCCSResultAll）実装

---
+++ class/uiRCCSResultSearchModel.php
@@ -0,0 +1,19 @@
+<?php
+
+class UiRCCSResultSearchModel {
+  // xmlで自動生成するときに順番通りに要素が並ぶ
+  public $cardStatus;
+  public $brandName; // カードブランド名
+  public $payType;
+  
+  public $fromDate;
+  public $toDate;
+  
+  public $orderNumber;
+  public $ownerName; // カード名義人
+  public $helpItem;
+  
+  function __construct() {
+
+  }
+}
